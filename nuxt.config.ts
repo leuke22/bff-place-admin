dc45878f88e2cb51cbbf779ad3639aeb117d5ad1
Keny Brian Amigable <kamigable@ccci-tech.com>
2026-08-18 19:28:10 +0800

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/hints',
+    '@nuxt/image',
+    '@artmizu/nuxt-prometheus',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@sidebase/nuxt-auth'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2026-06-30',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
